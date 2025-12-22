d3ada7db01af0816dd1a052d9def448839567a3b
Bedolaga69 <gomer121312@gmail.com>
2025-12-22 18:25:28 +0300

    sadfas
    fsdfa

--- game/character.py
+++ game/character.py
@@ -1,12 +1,14 @@
 from Items import *
 
 class Character:
-    def __init__(self, name, health, attack, defense):
+    def __init__(self, name, health, attack, defense, equipped_weapon = None, equipped_armor = None):
         self.name = name
         self.health = health
         self.attack = attack
         self.defense = defense
         self.inventory = []
+        self.equipped_weapon = equipped_weapon
+        self.equipped_armor = equipped_armor
 
     def get_damage(self, amount):
         actual_damage = max(amount - self.defense, 0)
@@ -21,10 +23,31 @@ class Character:
         other_character.get_damage(self.attack)
 
     def use_item(self, item):
+        if isinstance(item, Equipment):
+            if item in self.inventory:
+                print(f"{self.name} использует {item}")
+                if item.equipment_type == "weapon":
+                    if self.equipped_weapon is not None:
+                        old_weapon = self.equipped_weapon
+                        self.attack -= old_weapon.effect_value
+                        self.inventory.append(old_weapon)
+                    self.equipped_weapon = item
+                    self.attack += item.effect_value
+                    self.inventory.remove(item)
+                    print(f"вы экипировали {item.name}")
+                elif item.equipment_type == "armor":
+                    if self.equipped_armor is not None:
+                        old_armor = self.equipped_armor
+                        self.defense -= old_armor.effect_value
+                        self.inventory.append(old_armor)
+                    self.equipped_armor = item
+                    self.defense += item.effect_value
+                    self.inventory.remove(item)
+                    print(f"вы экипировали {item.name}")
+
         if isinstance(item, Consumable):
             if item in self.inventory:
                 print(f"{self.name} использует {item}")
-                self.inventory.remove(item)
                 if item.effect_type == "heal":
                     self.health += item.effect_value
                     print(f"текущее хп {self.name} = {self.health}")
@@ -34,8 +57,6 @@ class Character:
                 elif item.effect_type == "buff_attack":
                     self.attack += item.effect_value
                     print(f"текущая атака {self.name} = {self.attack}")
-            else:
-                print(f"предмет {item} не найден в инвентаре")
 
     def add_item(self, item):
         self.inventory.append(item)
@@ -46,11 +67,24 @@ item_type_list = ["potion", "weapon", "armor", "consumable"]
 effect_type_list = ["heal", "buff_attack", "buff_defense"]
 
 
-potion = Consumable("фласка", "восстанавливает 30 здоровья",95, "лечение", 40)
-armor = Equipment("броня", "дает 50 брони", "нагрудник", 50, 250)
-warrior = Character("воин", 100, 25, 10)
-maga = Character("маг", 80, 30, 25)
+potion = Consumable("фласка", "восстанавливает 30 здоровья",95, "heal", 40)
+armor = Equipment("броня", "дает 50 брони", "armor", 50, 250, 25)
+mage_armor = Equipment("мантия", "дает 100 брони", "armor", 125, 150, 50)
+leather_armor = Equipment("кожанные поножи", "дает 25 брони", "armor", 55, 45, 15)
+warrior = Character("воин", 100, 25, 10, None, armor)
+maga = Character("маг", 80, 30, 25, None, mage_armor)
 
-warrior.add_item("зелье здоровья")
+
+warrior.inventory.append(leather_armor)
+print(warrior.inventory[0])
+warrior.use_item(warrior.inventory[0])
+
+maga.use_item(mage_armor)
+maga.use_item(potion)
+
 warrior.attack_target(maga)
-warrior.use_item("зелье здоровья")
+warrior.use_item(potion)
+
+warrior.use_item(potion)
+
+print(warrior.inventory)
