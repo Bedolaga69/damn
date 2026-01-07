8f96048b343839016f103d9f0114deda0c4a693d
Bedolaga69 <gomer121312@gmail.com>
2026-01-07 17:56:00 +0300

    Update game.py
    дзшка

--- game/game.py
+++ game/game.py
@@ -1,5 +1,5 @@
-from Items import *
-from character import *
+from game_units import *
+
 
 class Game:
     def __init__(self, name, description):
@@ -9,121 +9,95 @@ class Game:
         self.enemies = []
         self.characters_templates = {}
         self.current_player = None
+        self.current_enemy = None
 
     def start_game(self):
         print("добро пожаловать в игру!")
         self.is_running = True
-        self.setup()
-        answer = input("выберите персонажа 1-3: воин - 1, маг - 2, лучник - 3")
-        if answer == "1":
-            print("вы выбрали война!")
-        elif answer == "2":
-            print("вы выбрали мага!")
-        elif answer == "3":
-            print("вы выбрали лучника!")
-        else:
-            print("попробуйте заново")
-
-
-    def setup(self):
         self.characters_templates = {
             "1": Character("Воин", 100, 20, 15),
             "2": Character("Маг", 70, 35, 5),
             "3": Character("Лучник", 85, 25, 10)}
+        answer = input("выберите персонажа 1-3: воин - 1, маг - 2, лучник - 3 :")
+        if answer == "1":
+            self.current_player = self.characters_templates["1"]
+            print("вы выбрали война!")
+        elif answer == "2":
+            self.current_player = self.characters_templates["2"]
+            print("вы выбрали мага!")
+        elif answer == "3":
+            self.current_player = self.characters_templates["3"]
+            print("вы выбрали лучника!")
+        else:
+            self.current_player = self.characters_templates["1"]
+            print("некорректный выбор, по умолчанию выбран воин")
+        self._create_default_enemies(4)
+        self._start_battle_loop()
 
-    def start_battle_loop(self):
-        pass
+
+    def _create_default_enemies(self, count):
+            """Приватный метод для создания набора врагов."""
+            enemy_templates = [
+                ("Гоблин", 80, 8, 15),
+                ("Орк", 100, 12, 30),
+                ("Скелет", 50, 10, 15)
+            ]
+
+            for i in range(count):
+                # Выбираем шаблон врага циклически
+                template = enemy_templates[i % len(enemy_templates)]
+                name, hp, atk, dfns = template
+                self.enemies.append(Enemy(f"{name} {i+1}", hp, atk, dfns))
+
+            print(f"вы оглянулись и увидели {len(self.enemies)} врагов")
+
+    def _start_battle_loop(self):
+        while self.current_player.is_alive() and len(self.enemies) > 0: #проверка на хп игрока и на то есть ли враги
+            self._process_player_action()# Вызываем ход игрока
+
+            if len(self.enemies) > 0:# проверка есть ли враги
+                self._process_enemy_turn()# если есть, то атакуют
+
+        if self.current_player.is_alive() <= 0:# проверка на хп игрока
+            print("вы проиграли... игра окончена!")
+            self.is_running = False
+        else:
+            print("победа! все враги повержены!")
+
+    def _process_player_action(self):
+        """метод для обработки выбора игрока"""
+        print(f"моя защита: {self.current_player.defense}, мое HP: {self.current_player.health}/{self.current_player.max_health}")
+        current_target = self.enemies[0]
+        print(f"Противник: {current_target.name} (HP: {current_target.health})")
+
+        answer = input("что вы хотите сделать?"
+                           "\n1 - атаковать"
+                           "\n2 - использовать предмет: ")
+
+        if answer == "1":
+            target = self.enemies[0]
+            self.current_player.attack_target(target)
+
+            if target.health <= 0:# проверка на живучесть цели после удара
+                print(f"враг {target.name} мертв")
+                self.enemies.pop(0)
+
+    def _process_enemy_turn(self):
+        """Метод для автоматического хода противника"""
+        attaker = self.enemies[0]
+        print(f"ход врага {attaker.name}")
+        attaker.attack_target(self.current_player)
+        self.enemies.pop(0)
 
     def stop_game(self):
         answer = input("вы действительно хотите выйти? "
-                       "да"
-                       "нет")
+                       "\nда"
+                       "\nнет: ")
         if answer == "да":
             self.is_running = False
         else:
             self.is_running = True
 
-class Unit:
-    def __init__(self, name, health, attack, defense):
-        self.name = name
-        self.health = health
-        self.attack = attack
-        self.defense = defense
-        self.base_defense = defense
-        self.base_attack = attack
 
-
-    def get_damage(self, amount):
-        actual_damage = max(amount - self.defense, 0)
-        if self.health <= actual_damage:
-            print(f"{self.name} повержен!")
-        else:
-            self.health -= actual_damage
-            print(f"{self.name} получает {actual_damage} урона, здоровье: {self.health}")
-
-    def attack_target(self, other_character):
-        print(f"{self.name} атакует {other_character.name}!")
-        other_character.get_damage(self.attack)
-
-class Character(Unit):
-    def __init__(self, name, health, attack, defense,  equipped_weapon = None, equipped_armor = None):
-        super().__init__(name, health, attack, defense)
-        self.inventory = []
-        self.equipped_weapon = equipped_weapon
-        self.equipped_armor = equipped_armor
-        self.base_defense = defense
-        self.base_attack = attack
-        self.defense = self.base_defense
-        if self.equipped_armor:
-            self.defense += self.equipped_armor.effect_value
-        self.attack = self.base_attack
-        if self.equipped_weapon:
-            self.attack += self.equipped_weapon.effect_value
-
-    def use_item(self, item):
-        if isinstance(item, Equipment):
-            if item in self.inventory:
-                print(f"{self.name} использует {item}")
-                if item.equipment_type == "weapon":
-                    if self.equipped_weapon is not None:
-                        old_weapon = self.equipped_weapon
-                        self.attack -= old_weapon.effect_value
-                        self.inventory.append(old_weapon)
-                    self.equipped_weapon = item
-                    self.attack += item.effect_value
-                    self.inventory.remove(item)
-                    print(f"вы экипировали {item.name}")
-                elif item.equipment_type == "armor":
-                    if self.equipped_armor is not None:
-                        old_armor = self.equipped_armor
-                        self.defense -= old_armor.effect_value
-                        self.inventory.append(old_armor)
-                    self.equipped_armor = item
-                    self.defense += item.effect_value
-                    self.inventory.remove(item)
-                    print(f"вы экипировали {item.name}")
-            else:
-                print(f"предмета {item.name} нет в инвентаре")
-
-        if isinstance(item, Consumable):
-            if item in self.inventory:
-                print(f"{self.name} использует {item}")
-                if item.effect_type == "heal":
-                    self.health += item.effect_value
-                    print(f"текущее хп {self.name} = {self.health}")
-                elif item.effect_type == "buff_defense":
-                    self.defense += item.effect_value
-                    print(f"текущая защита {self.name} = {self.defense}")
-                elif item.effect_type == "buff_attack":
-                    self.attack += item.effect_value
-                    print(f"текущая атака {self.name} = {self.attack}")
-            else:
-                print(f"предмета {item.name} нет в инвентаре")
-
-    def add_item(self, item):
-        self.inventory.append(item)
-        print(f"{item} добавлен в инвентарь {self.name}")
-
-class Enemy(Unit):
-    def __init__(self, name, health, defense, attack):
-        super().__init__(name, health, attack, defense)
+game = Game("подземелье колодца", "приключение в подземелье")
+game.start_game()
